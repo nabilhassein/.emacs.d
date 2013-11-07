c49e614440dfb5c91db856651716d1595b63e61f
Nabil Hassein <nabil@drdi.net>
2013-11-07 17:31:50 -0500

    took handy prelude-swap-windows function

--- keybindings.el
+++ keybindings.el
@@ -72,6 +72,25 @@
                                  (ibuffer)
                                  (local-unset-key (kbd "C-t"))))
 
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (if (/= (count-windows) 2)
+      (message "You need exactly 2 windows to do this.")
+    (let* ((w1 (car (window-list)))
+           (w2 (cadr (window-list)))
+           (b1 (window-buffer w1))
+           (b2 (window-buffer w2))
+           (s1 (window-start w1))
+           (s2 (window-start w2)))
+      (set-window-buffer w1 b2)
+      (set-window-buffer w2 b1)
+      (set-window-start w1 s2)
+      (set-window-start w2 s1)))
+  (other-window 1))
+
+(global-set-key (kbd "C-c s") 'swap-windows)
+
 ;;; Not keybindings exactly
 ;; M-x qrr performs query-replace-regexp
 (defalias 'qrr 'query-replace-regexp)
