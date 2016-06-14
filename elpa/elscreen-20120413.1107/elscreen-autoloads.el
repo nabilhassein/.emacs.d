7819e89afbf6be90cc3ffb23a18edefd659a2f10
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-14 17:21:43 -0400

    delete elpa files checked into git

--- elpa/elscreen-20120413.1107/elscreen-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; elscreen-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (elscreen-start) "elscreen" "elscreen.el" (21051
-;;;;;;  41594 972826 825000))
-;;; Generated autoloads from elscreen.el
-
-(autoload 'elscreen-start "elscreen" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("elscreen-pkg.el") (21051 41595 76823
-;;;;;;  729000))
-
-;;;***
-
-(provide 'elscreen-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; elscreen-autoloads.el ends here
