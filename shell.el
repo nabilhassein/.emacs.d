4e25dbc59c6006c9255e3dac6724bbcc63753e4b
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-13 22:43:31 -0400

    use zsh

--- shell.el
+++ shell.el
@@ -5,6 +5,13 @@
 (require 'shell-switcher)
 (setq shell-switcher-mode t)
 
+;; see http://rawsyntax.com/blog/learn-emacs-zsh-and-multi-term/
+(setq multi-term-program "/bin/zsh")
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)))
+
 (global-set-key (kbd "C-c C-z") (lambda ()
                                   (interactive)
                                   (multi-term)
