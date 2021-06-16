2602d9f5ea18fc535b1ddacb2e4f1c666ceaac18
Nabil Hassein <nabil.hassein@gmail.com>
2021-06-15 20:28:15 -0400

    minor i guess last commit not pushed was main reason for no shared calendar

--- org-mode.el
+++ org-mode.el
@@ -2,3 +2,4 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-font-lock-mode 1)
 (setq org-agenda-files (directory-files "~/../../notes/work/calendar" 'absolute "[^#]*.org$" 'no-sort))
+(add-hook 'org-mode-hook 'visual-line-mode)
